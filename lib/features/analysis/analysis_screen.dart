70e7fc0fa7e355efea66c97e3966dd85a19bb534
HagerHifnawy <164747180+Hhifnawy@users.noreply.github.com>
2025-07-30 17:42:03 +0300

    analysis chart

--- lib/features/analysis/analysis_screen.dart
+++ lib/features/analysis/analysis_screen.dart
@@ -13,86 +13,114 @@ class AnalysisScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: CustomAppBar(title: 'analysis'.tr(context),),
+    return Scaffold(
+      appBar: CustomAppBar(title: 'analysis'.tr(context)),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15.w,vertical: 15.h),
+        padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
         child: BlocBuilder<AnalysisCubit, AnalysisStates>(
-  builder: (context, state) {
-    return Column(
-          children: [
-            Text('analysis_title'.tr(context),  style: interRegular.copyWith(
-              color: AppColors.forthColor,
-              fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 18.sp),
-            ),),
-            const Divider(),
-            Text('figure1'.tr(context),  style: interRegular.copyWith(
-              color: AppColors.errorColor,
-              fontSize: SizeConfig.responsiveValue(phone: 12.sp, tablet: 16.sp),
-            ),),
+          builder: (context, state) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(
+                    'analysis_title'.tr(context),
+                    style: interRegular.copyWith(
+                      color: AppColors.forthColor,
+                      fontSize: SizeConfig.responsiveValue(
+                        phone: 14.sp,
+                        tablet: 18.sp,
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                  Text(
+                    'figure1'.tr(context),
+                    style: interRegular.copyWith(
+                      color: AppColors.errorColor,
+                      fontSize: SizeConfig.responsiveValue(
+                        phone: 12.sp,
+                        tablet: 16.sp,
+                      ),
+                    ),
+                  ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Flexible(
-                  child: Row(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Container(
-                        width: 20.w,
-                        height: 20.w,
-                        decoration: const BoxDecoration(
-                          color: AppColors.greenColor,
-                          shape: BoxShape.circle,
+                      Flexible(
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 20.w,
+                              height: 20.w,
+                              decoration: const BoxDecoration(
+                                color: AppColors.greenColor,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            6.horizontalSpace,
+                            Flexible(
+                              child: Text(
+                                'your_score'.tr(context),
+                                overflow: TextOverflow.ellipsis,
+                                style: interRegular.copyWith(
+                                  color: AppColors.forthColor,
+                                  fontSize: SizeConfig.responsiveValue(
+                                    phone: 14.sp,
+                                    tablet: 18.sp,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      6.horizontalSpace,
                       Flexible(
-                        child: Text(
-                          'your_score'.tr(context),
-                          overflow: TextOverflow.ellipsis,
-                          style: interRegular.copyWith(
-                            color: AppColors.forthColor,
-                            fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 18.sp),
-                          ),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 20.w,
+                              height: 20.w,
+                              decoration: const BoxDecoration(
+                                color: AppColors.blueColor,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                            6.horizontalSpace,
+                            Flexible(
+                              child: Text(
+                                'score_who_passed'.tr(context),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                                style: interRegular.copyWith(
+                                  color: AppColors.forthColor,
+                                  fontSize: SizeConfig.responsiveValue(
+                                    phone: 14.sp,
+                                    tablet: 18.sp,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-                Flexible(
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 20.w,
-                        height: 20.w,
-                        decoration: const BoxDecoration(
-                          color: AppColors.blueColor,
-                          shape: BoxShape.circle,
-                        ),
+                  if (context.read<AnalysisCubit>().analysisModel != null)
+                    SizedBox(
+                      height: 400.h,
+                      child: PerformanceChart(
+                        data: context
+                            .read<AnalysisCubit>()
+                            .analysisModel!
+                            .data!,
                       ),
-                      6.horizontalSpace,
-                      Flexible(
-                        child: Text(
-                          'score_who_passed'.tr(context),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          style: interRegular.copyWith(
-                            color: AppColors.forthColor,
-                            fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 18.sp),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            if(context.read<AnalysisCubit>().analysisModel!=null)
-            PerformanceChart(data: context.read<AnalysisCubit>().analysisModel!.data!,)
-          ],
-        );
-  },
-),
+                    ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
